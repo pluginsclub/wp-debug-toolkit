4d8c36d91fb0e2464d3da9f36f82d1b241723127
Stefan Pejcic <stefan@pejcic.rs>
2023-02-06 10:48:28 +0100

    v1.0 sync from SVN

---
+++ README.txt
@@ -0,0 +1,58 @@
+=== WP_DEBUG Toggle ===
+Contributors: stefanpejcic, pluginsclub
+Tags: WP_DEBUG, debug, error_log, enable wp debug, disable wpdebug
+Requires at least: 4.0
+Tested up to: 6.1
+Requires PHP: 5.4
+Stable tag: 1.0
+Donate link: https://plugins.club/wordpress/wp_debug-toggle/
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+== Description ==
+
+Toggle WP_DEBUG **🔴ON** or **🟢OFF** from the wp-admin bar.
+
+WP_DEBUG Toggle allows you to Enable [WP_DEBUG](https://wordpress.org/support/article/debugging-in-wordpress/) with one click to **log PHP, database and JavaScript errors** when you need to, and disable it when you're done. No need to manually edit wp-config.php file. 
+
+For more free WordPress plugins please visit [plugins.club](https://plugins.club/).
+
+== Installation ==
+
+1. Upload the plugin file to the `/wp-content/plugins/` directory.
+2. Activate the plugin through the 'Plugins' menu in WordPress.
+
+The current WP_DEBUG value will be shown in the top right corner of the admin top bar. Simply click on the value to change it.
+
+For basic usage, you can also have a look at the [plugin web site](https://plugins.club/).
+
+== Frequently Asked Questions ==
+
+= What is the current WP_DEBUG value? =
+
+The current WP_DEBUG value will be detected by the plugin and shown in the top right corner of the top bar on both frontened and wp-admin.
+
+= How to enable or disable WP_DEBUG ? =
+
+Simply click on the value to change it.
+
+= I have moved wp-config.php file, will this plugin still work? =
+
+Yes, If you have moved wp-config.php file one folder above according to the [Hardening WP guide](https://wordpress.org/documentation/article/hardening-wordpress/#securing-wp-config-php) this plugin will still detect the file there and work as normal. The only scenario when it will not work is if you renamed wp-config.php file to something else, for example: configuration.php
+
+= Is this plugin safe to use? =
+
+Yes, all the best development practices have been used and the plugin code is verified by WordPress.org Plugin Review Team.
+
+== Upgrade Notice ==
+
+This is new version 1.0
+
+== Screenshots ==
+
+1. Toggle WP_DEBUG from the toolbar
+ 
+== Changelog ==
+
+= 1.0 =
+The first stable public version.
---
+++ wp-debug-toggle.php
@@ -0,0 +1,106 @@
+<?php
+/*
+ * Plugin Name: WP_DEBUG Toggle
+ * Plugin URI: https://plugins.club/wordpress/wp_debug-toggle/
+ * Description: 🔴Enable / 🟢Disable WP_DEBUG from admin and update wp-config.php file.
+ * Version: 1.0
+ * Author: plugins.club
+ * Author URI: https://plugins.club/
+ */
+
+// Register and enqueue the plugin's stylesheet
+function wp_debug_toggle_enqueue_styles() {
+    wp_register_style( 'wp-debug-toggle', plugin_dir_url( __FILE__ ) . 'wp-debug-toggle.css' );
+    wp_enqueue_style( 'wp-debug-toggle' );
+}
+add_action( 'admin_enqueue_scripts', 'wp_debug_toggle_enqueue_styles' );
+add_action( 'wp_enqueue_scripts', 'wp_debug_toggle_enqueue_styles' );
+
+
+// Add the WP_DEBUG toggle button to the WP-Admin bar
+function add_wp_debug_toggle_to_admin_bar() {
+    // Get the current status of WP_DEBUG
+    $wp_debug = ( defined( 'WP_DEBUG' ) && WP_DEBUG === true );
+
+    // Set the label and CSS class for the button based on the current status
+    if ( $wp_debug ) {
+        $label = 'WP_DEBUG ON';
+        $class = 'wp-debug-on';
+    } else {
+        $label = 'WP_DEBUG OFF';
+        $class = 'wp-debug-off';
+    }
+
+    global $wp_admin_bar;
+    $wp_admin_bar->add_node( array(
+        'id'    => 'wp-debug-toggle',
+        'title' => $label,
+        'href'  => '#',
+        'meta'  => array( 'class' => $class ),
+        'parent' => 'top-secondary'
+    ) );
+}
+
+add_action( 'admin_bar_menu', 'add_wp_debug_toggle_to_admin_bar', 100 );
+
+// Handle the toggle button click
+function handle_wp_debug_toggle() {
+    // Check if the toggle button was clicked
+    if ( isset( $_GET['wp-debug-toggle'] ) ) {
+        // Get the path to the wp-config.php file
+        $config_path = ABSPATH . 'wp-config.php';
+        // Check if the wp-config.php file exist in the default location
+        if (!file_exists($config_path)) {
+            // If not, check if the wp-config file exist in the parent folder of the wp-config.php file as per https://wordpress.org/support/article/hardening-wordpress/#securing-wp-config-php
+            $config_path = dirname(ABSPATH) . '/wp-config.php';
+            if (!file_exists($config_path)) {
+                // If the wp-config.php file is not found in either of the above locations,
+                // return an error message
+                echo 'wp-config.php file could not be found. This plugin will not work if you are using a custom name for the wp-config.php file.';
+                return;
+            }
+        }
+        // Read the contents of the wp-config.php file
+        $config_contents = file_get_contents( $config_path );
+
+        // Get the current status of WP_DEBUG
+        $wp_debug = ( defined( 'WP_DEBUG' ) && WP_DEBUG === true );
+
+        // Update the value of WP_DEBUG in the wp-config.php file
+        if ( $wp_debug ) {
+            $config_contents = preg_replace( '/define\s*\(\s*\'WP_DEBUG\'\s*,\s*true\s*\)\s*;/', "define('WP_DEBUG', false);", $config_contents );
+        } else {
+            $config_contents = preg_replace( '/define\s*\(\s*\'WP_DEBUG\'\s*,\s*false\s*\)\s*;/', "define('WP_DEBUG', true);", $config_contents );
+        }
+
+        // Write the updated contents back to the wp-config.php file
+        file_put_contents( $config_path, $config_contents );
+
+        // Redirect the user back to the current page
+        wp_redirect( remove_query_arg( 'wp-debug-toggle' ) );
+        exit;
+    }
+}
+add_action( 'init', 'handle_wp_debug_toggle' );
+
+// Add the JavaScript to handle the toggle button click
+function add_wp_debug_toggle_script() {
+    ?>
+    <script>
+    jQuery(document).ready(function($) {
+        $('#wp-admin-bar-wp-debug-toggle a').click(function(e) {
+            e.preventDefault();
+            var href = $(this).attr('href');
+            if ( href !== '#' ) {
+                window.location = href;
+            } else {
+                window.location = '<?php echo add_query_arg( 'wp-debug-toggle', 1 ); ?>';
+            }
+        });
+    });
+    </script>
+    <?php
+}
+add_action( 'admin_footer', 'add_wp_debug_toggle_script' );
+add_action( 'wp_footer', 'add_wp_debug_toggle_script' );
+
